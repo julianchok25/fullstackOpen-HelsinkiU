4a87367a4ba1def7d7429925d4f1ddab32e4be8a
Julian Villegas <jville7@bancodebogota.com.co>
2023-09-27 23:12:06 -0500

    [UPD]App: Move all course info into an object. Excercise 1.5

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -3,30 +3,31 @@ import Header from './components/Header';
 import Total from './components/Total';
 
 const App = () => {
-	const course = 'Half Stack application development';
-
-	const parts = [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10,
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7,
-		},
-		{
-			name: 'State of a component',
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<>
-			<Header course={course} />
+			<Header course={course.name} />
 
-			<Content parts={parts} />
+			<Content parts={course.parts} />
 
-			<Total parts={parts} />
+			<Total parts={course.parts} />
 		</>
 	);
 };
